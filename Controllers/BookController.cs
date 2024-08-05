e42b9a87f4f222f24524d83d3b2c61f90f692a92
Pedro Fontoura <pedrfont@gmail.com>
2024-08-05 14:06:11 -0300

    updated crud views

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -58,7 +58,7 @@ namespace LibraryApp
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Synopsis,Status,CreatedAt,UpdatedAt")] Book book)
+        public async Task<IActionResult> Create([Bind("Id,Title,Synopsis,Status")] Book book)
         {
             if (ModelState.IsValid)
             {
@@ -90,7 +90,7 @@ namespace LibraryApp
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Synopsis,Status,CreatedAt,UpdatedAt")] Book book)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Synopsis,Status")] Book book)
         {
             if (id != book.Id)
             {
---
+++ Models/CreateBookViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace LibraryApp.Models;
+
+public class CreateBookViewModel
+{
+    public List<BookStatus> BookStatuses { get; set; }
+}
